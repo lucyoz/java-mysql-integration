ef606df27d3631c45e31378ece24f7764ee8a6a3
lucyoz <lucyoz.kwon@gmail.com>
2022-10-19 13:40:12 +0900

    spring 적용

--- src/main/java/com/dbexercise/dao/UserDaoFactory.java
+++ src/main/java/com/dbexercise/dao/UserDaoFactory.java
@@ -1,13 +1,19 @@
 package com.dbexercise.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
     //조립을 해줍니다.
+    @Bean
     public UserDao awsUserDao(){
         AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
         return userDao;
     }
 
+    @Bean
     public UserDao localUserDao(){
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- src/test/java/com/dbexercise/dao/UserDaoTest.java
+++ src/test/java/com/dbexercise/dao/UserDaoTest.java
@@ -3,17 +3,27 @@ package com.dbexercise.dao;
 import com.dbexercise.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        String id = "21";
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        String id = "23";
         userDao.add(new User(id, "NuNum","1123qqwe"));
 
         User user = userDao.findById(id);
